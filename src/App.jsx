4fece7208f99ff9c84c2ca135a72df17acfd8250
Aaron Ayeni <aaronayeni135@gmail.com>
2024-07-02 00:32:00 +0100

    functionality to fetch data from the RestCountriesAPI and store in the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import CountryList from './containers/CountryList';
+import VisitedCountries from './containers/VisitedCountries';
 import './App.css'
 
-function App() {
-  
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(response => response.json())
+      .then(data => setCountries(data));
+  }, []);
+
+  const markAsVisited = (country) => {
+    setVisitedCountries([...visitedCountries, country]);
+  };
 
   return (
+    <div className="App">
+      <h1>Country Bucket List</h1>
+      <CountryList countries={countries} markAsVisited={markAsVisited} />
+      <VisitedCountries visitedCountries={visitedCountries} />
+    </div>
     
-
   )
 }
 
