37e45d335daa88c29a3da8a75b813fd4574012f5
Aaron Ayeni <aaronayeni135@gmail.com>
2024-07-02 01:05:31 +0100

    added functionality to Country.jsx to create a hyperlink for each country when clicked, opens a Google Maps search for the specified country in a new browser tab showing more specific details of selected country

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -6,6 +6,7 @@ const Country = ({ country, markAsVisited }) => {
       <h2>{country.name.common}</h2>
       <p>Capital: {country.capital ? country.capital[0] : 'N/A'}</p>
       <p>Region: {country.region}</p>
+      <a href={`https://www.google.com/maps?q=${country.name.common}`} target="_blank">View on Map</a>
       <button onClick={() => markAsVisited(country)}>Mark as Visited</button>
     </div>
   );
